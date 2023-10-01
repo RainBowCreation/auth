a6393249ec6db55a40d39a8e0709dabef2c680ba
VocanicZ <ampnattahapat@gmail.com>
2023-10-02 02:21:20 +0700

    update

--- auth.py
+++ auth.py
@@ -1,8 +1,15 @@
+import os
 import sys
 import itertools
-import yaml
+import ruamel.yaml
 import mysql.connector as sql
 import mariadb
+
+plugins = ["RBCcore", "Lands", "LuckPerms"]
+yaml = ruamel.yaml.YAML()
+
+def getpath(plugin):
+    return "plugins/"+plugin+"/config.yml"
 
 def mysql(data:list):
     i = itertools.count(1)
@@ -54,12 +61,66 @@ def dinput():
     database = input('database (Skip will use default):')
     return [auth, user, password, host, port, database]
 
+def config_checker():
+    check = True
+    for path in plugins:
+        if (not os.path.exists(getpath(path))):
+            print(f"{path} not exists please check that the file or run ./start to recreate the files and try again.")
+            check = False
+    return check
+
+def rbccore(data):
+    i = itertools.count(0)
+    if (data[next(i)] != 'noauth'):
+        with open(getpath(plugins[0]), "r") as f:
+            config = yaml.load(f)
+            config['mySQL']['enable'] = True
+            config['mySQL']['username'] = data[next(i)]
+            config['mySQL']['password'] = data[next(i)]
+            config['mySQL']['url'] = data[next(i)]
+            if (data[0] == 'mariadb'):
+                config['mySQL']['url'] = "jdbc:mariadb//"+config['mySQL']['url']
+            config['mySQL']['port'] = data[next(i)]
+            config['mySQL']['database'] = data[next(i)]
+        with open(getpath(plugins[0]), "w") as f:
+            yaml.dump(config, f)
+       
+def lands(data):
+    i = itertools.count(0)
+    if (data[next(i)] == 'mysql'):
+        with open(getpath(plugins[1]), "r") as f:
+            config = yaml.load(f)
+            config['database']['mysql']['enabled_20'] = True
+            config['database']['mysql']['username'] = data[next(i)]
+            config['database']['mysql']['password'] = data[next(i)]
+            config['database']['mysql']['ip-address'] = data[next(i)]
+            config['database']['mysql']['port'] = data[next(i)]
+            config['database']['mysql']['database'] = data[next(i)]
+        with open(getpath(plugins[1]), "w") as f:
+            yaml.dump(config, f)
+
+def luckperms(data):
+    i = itertools.count(0)
+    if (data[next(i)] != 'noauth'):
+        with open(getpath(plugins[2]), "r") as f:
+            config = yaml.load(f)
+            match(data[0]):
+                case('mysql'):
+                    config['storage-method'] = 'MySQL'
+                case('mariadb'):
+                    config['storage-method'] = 'MariaDB'
+            config['database']['database']['username'] = data[next(i)]
+            config['database']['database']['password'] = data[next(i)]
+            config['database']['database']['address'] = data[next(i)]+":"+data[next(i)]
+            config['database']['database']['database'] = data[next(i)]
+        with open(getpath(plugins[2]), "w") as f:
+            yaml.dump(config, f)
+
 def main():
     data = dinput()
     try:
         match(data[0]):
             case('noauth'):
-                #do no auth
                 print("Using noauth you can start the stanalone server using ./start.sh but the server will not connected to RainBowCreationMainNet!")
                 return
             case('mysql'):
@@ -69,14 +130,19 @@ def main():
     except Exception as e:
         print(f"Error: {e}")
         sys.exit(1)
-
-    # Get Cursor
     cur = conn.cursor()
     cur.execute(
         "SELECT * FROM ping")
     for (ping) in cur:
         if (ping[0] == 'pong'):
             print("connected")
-            #do the thing 
-            
+        else:
+            print("Cant connect to auth server..")
+            return
+    if (not config_checker()):
+        return
+    rbccore(data)
+    lands(data)
+    luckperms(data)
+    
 main()
